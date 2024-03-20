4a95c0fcee57ad2279c4b514ccf590ffe5817ff0
yp1210 <1639282391@qq.com>
2024-03-20 21:27:55 +0800

    feat: 页面布局，layout初始化请求数据

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { TabBar } from 'antd-mobile';
+import {
+  AppOutline,
+  MessageOutline,
+  MessageFill,
+  UnorderedListOutline,
+  UserOutline,
+} from 'antd-mobile-icons';
+import './index.scss';
+import { useDispatch } from 'react-redux'
+import { asyncGetBillList } from '@/store/bill';
 
 const Layout = () => {
-  return <div>
-    this is layout
-    <Outlet />
+  const dispatch = useDispatch();
+
+  const tabs = [
+    {
+      key: 'home',
+      title: '首页',
+      icon: <AppOutline />,
+    },
+    {
+      key: 'todo',
+      title: '待办',
+      icon: <UnorderedListOutline />,
+    },
+    {
+      key: 'message',
+      title: '消息',
+      icon: (active) =>
+        active ? <MessageFill /> : <MessageOutline />,
+    },
+    {
+      key: 'personalCenter',
+      title: '我的',
+      icon: <UserOutline />,
+    },
+  ]
+
+  useEffect(() => {
+    dispatch(asyncGetBillList())
+  }, [dispatch])
+
+  return <div className='layout'>
+    <div className='container'>
+      <Outlet />
+    </div>
+    <div className='footer'>
+      <TabBar>
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </div>
   </div>
 }
 
--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
 
 const Month = () => {
   return <div>
     this is month
+    <Button style={{'--background-color': 'blue', '--text-color': 'black'}} color='primary'>测试</Button>
   </div>
 }
 
