3a759821d7f2c9d9065a2f8872d16ccbe373e9bd
yp1210 <1639282391@qq.com>
2024-03-20 20:43:36 +0800

    feat: 添加layout组件

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Month from './pages/Month';
 import Year from './pages/Year';
 import New from './pages/New';
+import Layout from './pages/Layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Layout />,
     children: [
       {
         index: true,
